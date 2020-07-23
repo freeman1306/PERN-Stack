331ac2734401bd88fade7c127f1e96d0b4d09aad
freeman1306 <zavalkin06@gmail.com>
2020-07-23 18:00:00 +0300

    done

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,28 +1,58 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function EditTodo() {
+function EditTodo({ todo }) {
+    
+
+const [description, setDiscription] = useState(todo.description)
+
+// edit description function
+    
+    const updateDescription = async e => {
+        e.preventDefault()
+        try {
+            const body = { description }
+            const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
+                method: 'PUT',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
+            })
+
+            window.location = '/'
+        } catch (err) {
+            console.error(err);
+            
+        }
+    }
+
+
+
+    
     return (
         <div>
-<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">
+<button type="button" className="btn btn-warning" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
   Edit
 </button>
 
 
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Edit Todo</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div className="modal fade"
+                id={`id${todo.todo_id}`}
+                tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"
+                onClick= {() =>setDiscription( todo.description)}
+             >
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Edit Todo</h5>
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => setDiscription(todo.description)}>
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        <input type="text" className="form-control"/>
+      <div className="modal-body">
+                            <input type="text" className="form-control" value={description} onChange={e =>setDiscription( e.target.value)}/>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-warning">Edit</button>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-warning" onClick={updateDescription}>Edit</button>
       </div>
     </div>
   </div>
--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -55,7 +55,7 @@ const ListTodo = () => {
                     {todos.map(todo => (
                         <tr className="align-items-center">
                             <td>{todo.description}</td>
-                          <td> <EditTodo /></td>
+                            <td> <EditTodo todo={todo}/></td>
                             <td>
                             <button className="btn btn-danger"
                                 onClick={() => deleteTodo(todo.todo_id)}
